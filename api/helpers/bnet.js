d3d216b80b3e7b8bb419c38252cf37a9607bc5af
Michael Feist <mdfeist@ualberta.ca>
2020-07-31 12:19:37 -0600

    Add helper functions for battle net tag.

---
+++ api/helpers/bnet.js
@@ -0,0 +1,36 @@
+/*
+Regex for battle net tag:
+(^([A-zÀ-ú][A-zÀ-ú0-9]{2,11})|(^([а-яёА-ЯЁÀ-ú][а-яёА-ЯЁ0-9À-ú]{2,11})))(#[0-9]{4,})$
+
+Created by: https://eu.forums.blizzard.com/en/blizzard/t/battle-tag-regex-expression/444
+
+Rules:
+ - Numbers allowed, except for the first Letter
+ - Latin Alphabet or Cyrillic alphabet, but not a mixture
+ - Accented Letters are allowed in both situations
+ - No spaces or punctuations
+ - No Special Characters e.g. #, -, …
+ - 3-12 Characters
+ - The tag is a 4-5 numbers
+
+Explanation:
+ - 2 Groups: 1 for Latin Alphabet and 1 for Cyrillic Alphabet
+ - The first character can't be a number
+ - The Latin used A-z and À-ú
+ - The Cyrillic alphabet used а-я, А-Я, ё, Ё, and À-ú.
+*/
+const BNET_PATTERN = new RegExp(/(^([A-zÀ-ú][A-zÀ-ú0-9]{2,11})|(^([а-яёА-ЯЁÀ-ú][а-яёА-ЯЁ0-9À-ú]{2,11})))(#[0-9]{4,})$/);
+
+module.exports.checkValidBnet = (bnet) => {
+    return BNET_PATTERN.test(bnet);
+};
+
+module.exports.getBnetName = (bnet) => {
+    let results = BNET_PATTERN.exec(bnet);
+    return results[1];
+};
+
+module.exports.getBnetTag = (bnet) => {
+    let results = BNET_PATTERN.exec(bnet);
+    return results[5].substr(1);
+};

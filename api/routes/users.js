d42c96012b14f2123fec34ab679663c1e89538f9
Michael Feist <mdfeist@ualberta.ca>
2020-08-01 19:22:36 -0600

    Refactor user routing to use async functions.

--- api/routes/users.js
+++ api/routes/users.js
@@ -19,107 +19,95 @@ const router = express.Router();
 const serverConfig = config.get('SERVER');
 const jwtConfig = config.get('JWT');
 
-router.post('/signup', (req, res, next) => {
-    User.find({email: req.body.email})
-        .exec()
-        .then(user => {
-            if (user.length >= 1) {
-                res.statusCode = 409;
+router.post('/signup', async (req, res, next) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
 
-                const response = errorMessage(
-                    409,
-                    'unable to create user as email already exists'
-                );
+        const users = await User.find({email: email});
 
-                return res.json(response);
-            } else {
-                bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-                    if (err) {
-                        console.log(err);
-                        return res.status(500).json(errorMessage());
-                    } else {
-                        const user = new User({
-                            _id: new mongoose.Types.ObjectId(),
-                            email: req.body.email,
-                            password: hash
-                        });
-                        
-                        user.save()
-                            .then(result => {
-                                res.status(201).json({
-                                    message: 'user created',
-                                    userId: user._id
-                                });
-                            })
-                            .catch(err => {
-                                console.log(err);
-                                res.status(500).json(errorMessage());
-                            });
-                    }
-                });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(errorMessage());
+        if (users.length >= 1) {
+            const response = errorMessage(
+                409,
+                'unable to create user as email already exists'
+            );
+
+            return res.status(409).json(response);
+        }
+        
+        const hash = await bcrypt.hash(password, saltRounds);
+
+        const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: email,
+            password: hash
         });
+
+        const savedUser = await user.save();
+
+        return res.status(201).json({
+            message: 'user created',
+            userId: savedUser._id
+        });
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json(errorMessage());
+    }
 });
 
-router.post('/login', (req, res, next) => {
-    User.find({email: req.body.email})
-        .exec()
-        .then(user => {
-            if (user.length < 1) {
-                return res.status(401).json(authenticationErrorMessage());
-            } else {
-                bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                    if (err) {
-                        return res.status(401).json(authenticationErrorMessage());
-                    }
+router.post('/login', async (req, res, next) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
 
-                    if (result) {
-                        const token = jwt.sign({
-                            email: user[0].email,
-                            userId: user[0]._id
-                        },
-                        jwtConfig.KEY,
-                        {
-                            expiresIn: "1h"
-                        });
+        const users = await User.find({email: email});
 
-                        return res.status(200).json({
-                            message: 'authentication successful',
-                            jwt_token: token
-                        });
-                    } else {
-                        return res.status(401).json(authenticationErrorMessage());
-                    }
-                });
-            }
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(errorMessage());
-        });
-});
+        if (users.length < 1) {
+            return res.status(401).json(authenticationErrorMessage());
+        }
 
-router.delete('/:userId', checkDevAuth, (req, res, next) => {
-    // Get team id
-    const userId = req.params.userId;
-
-    // Remove user based off id
-    User.deleteOne({_id: userId})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: `Delete user with id: ${userId}`,
-                result: result
+        const user = users[0];
+        const match = await bcrypt.compare(password, user.password);
+    
+        if (match) {
+            const token = jwt.sign({
+                email: user.email,
+                userId: user._id
+            },
+            jwtConfig.KEY,
+            {
+                expiresIn: "1h"
             });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(errorMessage());
+
+            return res.status(200).json({
+                message: 'authentication successful',
+                jwt_token: token
+            });
+        } else {
+            return res.status(401).json(authenticationErrorMessage());
+        }
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json(errorMessage());
+    }
+});
+
+router.delete('/:userId', checkDevAuth, async (req, res, next) => {
+    try {
+        // Get team id
+        const userId = req.params.userId;
+
+        // Remove user based off id
+        const result = await User.deleteOne({_id: userId});
+
+        return res.status(200).json({
+            message: `Delete user with id: ${userId}`,
+            result: result
         });
+    }catch(err) {
+        console.log(err);
+        res.status(500).json(errorMessage());
+    }
 });
 
 module.exports = router;
